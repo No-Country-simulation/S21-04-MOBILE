29e3091e195b17a01ba1962b1f0fef97aa3c4d45
Slisandro <salvareschilisandro0@gmail.com>
2025-02-24 09:23:54 -0300

    DTO's

---
+++ Backend/src/models/Like.js
@@ -0,0 +1,8 @@
+class LikeDTO {
+    constructor(id, userId, postId, createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.postId = postId;
+        this.createdAt = createdAt;
+    }
+}  
