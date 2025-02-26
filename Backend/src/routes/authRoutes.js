40a8cd9d514527b2bca3120180c83db1f447dfd8
Lisandro <salvareschilisandro0@gmail.com>
2025-02-26 12:18:43 -0300

    Update authRoutes.js

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 
 router.post('/login', authController.login);
 router.post('/register', authController.register);
+router.delete('/:id', authController.deleteUser);
 router.post('/verify-token', authController.verifyToken);
 
 module.exports = router;
