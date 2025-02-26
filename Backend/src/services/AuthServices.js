3336dcc7ef1c8fc6d1f86780b5e32247c385acba
Lisandro <salvareschilisandro0@gmail.com>
2025-02-26 10:11:46 -0300

    Update AuthServices.js

--- Backend/src/services/AuthServices.js
+++ Backend/src/services/AuthServices.js
@@ -2,7 +2,7 @@ const database = require("../config/db");
 const UserDTO = require("../models/User");
 const bcrypt = require('bcryptjs');
  
-class ContactService {
+class AuthService {
     async getUser(userId) {
         const [result] = await database.query("SELECT * FROM user WHERE id = ?", [userId]);
 
@@ -19,38 +19,42 @@ class ContactService {
 
     async registerUser(email, password) {
         try {
-            const [userResult] = await database.query("SELECT id from user WHERE email = ?", [email]);
+          const [userResult] = await database.query("SELECT id from user WHERE email = ?", [email]);
 
-        if(result.length !== 0) {
+          if(result.length !== 0) {
             // usuario ya existe
             const error = new Error("El usuario ya existe");
             error.statusCode = 409;
             throw error;
-        }
+          }
 
-        const salt = bcrypt.genSaltSync(10);
-        const passwordHash = bcrypt.hashSync(password, salt);
+          const salt = bcrypt.genSaltSync(10);
+          const passwordHash = bcrypt.hashSync(password, salt);
 
-        const query = `
+          const query = `
             INSERT INTO user (email, password, created_at)
             VALUES (?, ?, ?)
-        `;
+          `;
 
-        const values = [
-            email, 
-            passwordHash,
-            new Date()
-        }
+          const values = [
+              email, 
+              passwordHash,
+              new Date()
+          }
 
-        const [insertResult] = await database.query(query, values);
+          const [insertResult] = await database.query(query, values);
 
-        return new UserDTO(insertResult.insertId, email);
+          return new UserDTO(insertResult.insertId, email);
         } catch (error) { 
-             throw error;
+           throw error;
         }
     }
 
     async login(email, password) {
+        try {
+           
+        } catch (error) {
 
+        }
     }
 }
