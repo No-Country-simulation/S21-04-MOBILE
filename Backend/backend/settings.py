38964694892887376a11163caaf1b9721f4115d2
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2025-03-10 14:52:54 -0300

    Update settings.py

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-trei.onrender.com', 'localhost:8000']
 
 
 # Application definition
