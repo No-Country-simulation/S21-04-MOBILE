d8bb6765bb4a784b115ff0acbf72c54bfde455be
Leandro Sánchez <leanmsan@gmail.com>
2025-03-11 19:35:54 -0300

    Correcciones de errores
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- Backend/posts/views.py
+++ Backend/posts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import AllowAny
 from .models import Post, Sample, Idea, Normal
 from .serializers import PostSerializer, PostCreateSerializer
@@ -8,6 +10,8 @@ from .serializers import PostSerializer, PostCreateSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['user__id', 'title', 'content']
 
     def get_serializer_class(self):
         if self.action == 'create' or self.action == 'update':
